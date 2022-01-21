36b76c0f13225d0d5397c22cbc864039c1fc4372
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-21 23:40:37 +0900

    remove unnecessary argument

--- classes/memo.rb
+++ classes/memo.rb
@@ -5,9 +5,9 @@ require 'time'
 class Memo
   attr_accessor :id, :title, :body
 
-  def initialize(title, body, time = Time.now)
+  def initialize(title, body)
     # idをユニークにするための処理
-    @id = Digest::MD5.hexdigest(title + body + time.to_s)
+    @id = Digest::MD5.hexdigest(title + body + Time.now.to_s)
     @title = title
     @body = body
   end
