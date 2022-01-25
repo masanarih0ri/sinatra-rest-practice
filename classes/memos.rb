a5cd7a1ece565c4c91aab25d952662d105775bd6
masanarih0ri <masanarihoridevelop@gmail.com>
2022-01-25 09:36:52 +0900

    fix security problem

--- classes/memos.rb
+++ classes/memos.rb
@@ -14,7 +14,8 @@ class Memos
     connection = PG.connect(dbname: 'memoapp')
     @memos = []
 	  query = "SELECT * FROM #{TABLE_NAME}"
-    results = connection.exec(query)
+    connection.prepare('all', query)
+    results = connection.exec_prepared('all')
     results.map { |result| @memos << result }
     @memos.sort {|a, b| a['created_at'] <=> b['created_at'] }
 	end
@@ -23,7 +24,8 @@ class Memos
     memo = []
     connection = PG.connect(dbname: 'memoapp')
     query = "SELECT * FROM #{TABLE_NAME} WHERE id = '#{id}'"
-    result = connection.exec(query)
+    connection.prepare('find', query)
+    result = connection.exec_prepared('find')
     result.map { |x| memo << x }
     memo[0]
   end
@@ -31,20 +33,21 @@ class Memos
   def self.add(memo)
     connection = PG.connect(dbname: 'memoapp')
 		query = "INSERT INTO #{TABLE_NAME} (id, title, body, created_at) VALUES ('#{memo.id}', '#{memo.title}', '#{memo.body}', '#{memo.created_at}')"
-	  connection.exec(query)
-    connection.finish
+    connection.prepare('add', query)
+	  connection.exec_prepared('add')
 	end
 
   def self.update(memo)
     connection = PG.connect(dbname: 'memoapp')
 		query = "UPDATE #{TABLE_NAME} SET title = '#{memo['title']}', body = '#{memo['body']}' WHERE id = '#{memo['id']}'"
-	  connection.exec(query)
-    connection.finish
+    connection.prepare('update', query)
+	  connection.exec_prepared('update')
 	end
 
   def self.delete(id)
     connection = PG.connect(dbname: 'memoapp')
     query = "DELETE FROM #{TABLE_NAME} WHERE id = '#{id}'"
-    connection.exec(query)
+    connection.prepare('delete', query)
+    connection.exec_prepared('delete')
   end
 end
