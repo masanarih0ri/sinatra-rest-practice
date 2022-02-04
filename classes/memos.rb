bc9813036c58b51d49bff5ba35800bd2af16b1fa
masanarih0ri <masanarihoridevelop@gmail.com>
2022-02-04 23:20:20 +0900

    fix end indent

--- classes/memos.rb
+++ classes/memos.rb
@@ -14,7 +14,7 @@ class Memos
     connection.prepare('all', "SELECT * FROM #{TABLE_NAME} ORDER BY created_at")
     results = connection.exec_prepared('all')
     @memos = results.to_a
-	end
+  end
 
   def self.find(id, connection)
     connection.prepare('find', "SELECT * FROM #{TABLE_NAME} WHERE id = $1")
@@ -26,12 +26,12 @@ class Memos
   def self.add(memo, connection)
     connection.prepare('add', "INSERT INTO #{TABLE_NAME} (id, title, body, created_at) VALUES ($1, $2, $3, $4)")
     connection.exec_prepared('add', ["#{memo.id}", "#{memo.title}", "#{memo.body}", "#{memo.created_at}"])
-	end
+  end
 
   def self.update(memo, connection)
     connection.prepare('update', "UPDATE #{TABLE_NAME} SET title = $1, body = $2 WHERE id = $3")
     connection.exec_prepared('update', ["#{memo['title']}", "#{memo['body']}", "#{memo['id']}"])
-	end
+  end
 
   def self.delete(id, connection)
     connection.prepare('delete', "DELETE FROM #{TABLE_NAME} WHERE id = $1")
